13c5c958dd44adc043c6a61213c6a08e9dfd7ad9
Senevirathna12 <anishantha702@gmail.com>
2024-02-16 15:16:26 +0530

    dog function added

--- animal.py
+++ animal.py
@@ -1,18 +1,19 @@
 import sys
 
-def cat():
-    print('Meow')
+def dog():
+    print('Baw')
 
 def default():
     print('Hello')
 
 def main():
-    if sys.argv[1] == 'cat':
-        cat()
+    if sys.argv[1] == 'dog':
+        dog()
     else:
         default()
 
 if __name__ == '__main__':
     main()
+
  
 
